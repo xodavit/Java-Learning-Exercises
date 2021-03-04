181ed796ca2e88dcff1af1734f1db6fb2b16dfbb
vitaliy_khodataev <khodataev.v@yandex.ru>
2021-03-04 18:56:08 +0300

    add spring type of wiring project

---
+++ tasks/courses/jm/springTypesOfWiring/src/main/java/koschei/models/Duck5.java
@@ -0,0 +1,21 @@
+package koschei.models;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Duck5 {
+
+
+    private Egg6 egg;
+
+    @Autowired
+    public Duck5(Egg6 egg) {
+        this.egg = egg;
+    }
+
+    @Override
+    public String toString() {
+        return ", в утке яйцо " + egg.toString();
+    }
+}
